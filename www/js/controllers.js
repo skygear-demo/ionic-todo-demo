7c6d46c53404185fd17acce3685dceb1f0810105
TakLee96 <jiahang_li@outlook.com>
2016-09-04 16:50:01 -0700

    Import Skygear JS SDK

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,12 @@
 angular.module('app.controllers', [])
   
-.controller('logInCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('logInCtrl', ['$scope', '$stateParams', '$skygear',
+// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $skygear) {
 
+    console.log($skygear);
 
 }])
    
--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,11 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
-
+.factory('$skygear', [function(){
+    skygear.config({
+        'apiKey': '94a9d05a1b6146c3a7455cb3f146546c',
+        'endPoint': 'https://todo.staging.skygeario.com/'
+    });
+    return skygear;
 }])
 
 .service('BlankService', [function(){
