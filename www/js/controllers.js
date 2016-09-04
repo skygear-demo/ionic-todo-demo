8a52445f361fafede56e9badef2056336d132968
TakLee96 <jiahang_li@outlook.com>
2016-09-04 16:43:34 -0700

    Create new ionic project and import creator content

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('logInCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signUpCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('toDoListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addItemCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editItemCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('itemDetailCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('logIn', {
+    url: '/login',
+    templateUrl: 'templates/logIn.html',
+    controller: 'logInCtrl'
+  })
+
+  .state('signUp', {
+    url: '/signup',
+    templateUrl: 'templates/signUp.html',
+    controller: 'signUpCtrl'
+  })
+
+  .state('toDoList', {
+    url: '/todo-list',
+    templateUrl: 'templates/toDoList.html',
+    controller: 'toDoListCtrl'
+  })
+
+  .state('addItem', {
+    url: '/add-item',
+    templateUrl: 'templates/addItem.html',
+    controller: 'addItemCtrl'
+  })
+
+  .state('editItem', {
+    url: '/edit-item',
+    templateUrl: 'templates/editItem.html',
+    controller: 'editItemCtrl'
+  })
+
+  .state('itemDetail', {
+    url: '/item-detail',
+    templateUrl: 'templates/itemDetail.html',
+    controller: 'itemDetailCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
