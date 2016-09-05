332116928e0a5316e0127686bb69896e8c0cdf54
TakLee96 <jiahang_li@outlook.com>
2016-09-04 17:00:32 -0700

    Implemented Log In page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,30 @@
 angular.module('app.controllers', [])
   
-.controller('logInCtrl', ['$scope', '$stateParams', '$skygear',
+.controller('logInCtrl', ['$scope', '$stateParams', '$skygear', '$ionicPopup', '$state',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $skygear) {
+function ($scope, $stateParams, $skygear, $ionicPopup, $state) {
 
-    console.log($skygear);
+    $scope.user = {};
+    $scope.login = function () {
+        if ($scope.user.username && $scope.user.password) {
+            $skygear.loginWithUsername($scope.user.username, $scope.user.password).then(function () {
+                $state.go('toDoList');
+            }, function (err) {
+                console.error(err);
+                $ionicPopup.alert({
+                    title: 'ERROR ' + err.error.code,
+                    template: err.error.message
+                });
+            });
+        } else {
+            $ionicPopup.alert({
+                title: 'ERROR',
+                template: 'Please fill in all the fields'
+            });
+        }
+    };
 
 }])
    
