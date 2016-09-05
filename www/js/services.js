dcb15adebba0a8f17dcaafb04c670225e853fd1b
TakLee96 <jiahang_li@outlook.com>
2016-09-04 18:06:01 -0700

    Implemented $items service

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('app.services', [])
 
-.factory('$skygear', [function(){
+.factory('$skygear', [function () {
     skygear.config({
         'apiKey': '94a9d05a1b6146c3a7455cb3f146546c',
         'endPoint': 'https://todo.staging.skygeario.com/'
@@ -8,6 +8,40 @@ angular.module('app.services', [])
     return skygear;
 }])
 
-.service('BlankService', [function(){
-
+.service('$items', ['$skygear', function ($skygear) {
+    var that = this;
+    var Item = $skygear.Record.extend('item');
+    this.items = [];
+    this.update = function () {
+        if ($skygear.currentUser) {
+            var query = new $skygear.Query(Item);
+            query.equalTo('_owner_id', $skygear.currentUser.id)
+            query.addDescending('_created_at');
+            return $skygear.publicDB.query(query).then(function (records) {
+                that.items = records;
+                that._onUpdate(records);
+            }, function (err) {
+                console.error(err);
+            });
+        }
+    };
+    this._onUpdate = function () {};
+    this.onUpdate = function (callback) {
+        that._onUpdate = callback;
+    }
+    this.create = function (item) {
+        $skygear.publicDB.save(new Item(item)).then(function (record) {
+            that.items.push(record);
+            that._onUpdate(that.items);
+        }, function (err) {
+            console.error(err);
+        });
+    };
+    this.sync = function (index) {
+        return $skygear.publicDB.save(that.items[index]).then(function () {
+            that._onUpdate(that.items);
+        }, function (err) {
+            console.error(err);
+        });
+    };
 }]);
